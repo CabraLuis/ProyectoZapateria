f942f7b5909a474d419b5b97cb46a575280b6224
Fermin921 <ferminmartinez921@gmail.com>
2023-07-14 21:43:38 -0500

    Funcionalidad Login

--- ZapateriaForm.Designer.cs
+++ ZapateriaForm.Designer.cs
@@ -35,18 +35,20 @@
             btnIngresar = new Button();
             btnSalir = new Button();
             lblBienvenida = new Label();
+            grpInicioSesion = new GroupBox();
+            grpInicioSesion.SuspendLayout();
             SuspendLayout();
             // 
             // TxtUsuario
             // 
-            TxtUsuario.Location = new Point(174, 79);
+            TxtUsuario.Location = new Point(221, 69);
             TxtUsuario.Name = "TxtUsuario";
             TxtUsuario.Size = new Size(125, 27);
             TxtUsuario.TabIndex = 0;
             // 
             // TxtContrasena
             // 
-            TxtContrasena.Location = new Point(174, 127);
+            TxtContrasena.Location = new Point(221, 117);
             TxtContrasena.Name = "TxtContrasena";
             TxtContrasena.Size = new Size(125, 27);
             TxtContrasena.TabIndex = 0;
@@ -54,7 +56,7 @@
             // lblUsuario
             // 
             lblUsuario.AutoSize = true;
-            lblUsuario.Location = new Point(82, 86);
+            lblUsuario.Location = new Point(129, 76);
             lblUsuario.Name = "lblUsuario";
             lblUsuario.Size = new Size(62, 20);
             lblUsuario.TabIndex = 1;
@@ -63,7 +65,7 @@
             // lblContrasena
             // 
             lblContrasena.AutoSize = true;
-            lblContrasena.Location = new Point(82, 134);
+            lblContrasena.Location = new Point(129, 124);
             lblContrasena.Name = "lblContrasena";
             lblContrasena.Size = new Size(86, 20);
             lblContrasena.TabIndex = 1;
@@ -71,47 +73,60 @@
             // 
             // btnIngresar
             // 
-            btnIngresar.Location = new Point(44, 172);
+            btnIngresar.Location = new Point(91, 162);
             btnIngresar.Name = "btnIngresar";
             btnIngresar.Size = new Size(94, 29);
             btnIngresar.TabIndex = 2;
             btnIngresar.Text = "Ingresar";
             btnIngresar.UseVisualStyleBackColor = true;
+            btnIngresar.Click += btnIngresar_Click;
             // 
             // btnSalir
             // 
-            btnSalir.Location = new Point(253, 172);
+            btnSalir.Location = new Point(300, 162);
             btnSalir.Name = "btnSalir";
             btnSalir.Size = new Size(94, 29);
             btnSalir.TabIndex = 3;
             btnSalir.Text = "Salir";
             btnSalir.UseVisualStyleBackColor = true;
+            btnSalir.Click += btnSalir_Click;
             // 
             // lblBienvenida
             // 
             lblBienvenida.AutoSize = true;
-            lblBienvenida.Location = new Point(88, 30);
+            lblBienvenida.Location = new Point(129, 23);
             lblBienvenida.Name = "lblBienvenida";
             lblBienvenida.Size = new Size(0, 20);
             lblBienvenida.TabIndex = 4;
+            // 
+            // grpInicioSesion
+            // 
+            grpInicioSesion.Controls.Add(btnSalir);
+            grpInicioSesion.Controls.Add(lblBienvenida);
+            grpInicioSesion.Controls.Add(TxtUsuario);
+            grpInicioSesion.Controls.Add(TxtContrasena);
+            grpInicioSesion.Controls.Add(btnIngresar);
+            grpInicioSesion.Controls.Add(lblUsuario);
+            grpInicioSesion.Controls.Add(lblContrasena);
+            grpInicioSesion.Location = new Point(25, 12);
+            grpInicioSesion.Name = "grpInicioSesion";
+            grpInicioSesion.Size = new Size(521, 206);
+            grpInicioSesion.TabIndex = 5;
+            grpInicioSesion.TabStop = false;
+            grpInicioSesion.Text = "Ingresar Datos:";
             // 
             // ZapateriaForm
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(399, 266);
-            Controls.Add(lblBienvenida);
-            Controls.Add(btnSalir);
-            Controls.Add(btnIngresar);
-            Controls.Add(lblContrasena);
-            Controls.Add(lblUsuario);
-            Controls.Add(TxtContrasena);
-            Controls.Add(TxtUsuario);
+            ClientSize = new Size(560, 233);
+            Controls.Add(grpInicioSesion);
             Name = "ZapateriaForm";
             Text = "Login";
             Load += ZapateriaForm_Load;
+            grpInicioSesion.ResumeLayout(false);
+            grpInicioSesion.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -123,5 +138,6 @@
         private Button btnIngresar;
         private Button btnSalir;
         private Label lblBienvenida;
+        private GroupBox grpInicioSesion;
     }
 }
--- ZapateriaForm.cs
+++ ZapateriaForm.cs
@@ -1,4 +1,12 @@
+
+using System;
+using System.Windows.Forms;
+using System.Data.SqlClient; //LIBIRERIA PARA CONECTAR BASE DE DATOS SQL
+using System.Net.Sockets;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
 namespace ProyectoZapateria
+
 {
     public partial class ZapateriaForm : Form
     {
@@ -9,21 +17,76 @@ namespace ProyectoZapateria
 
         private void ZapateriaForm_Load(object sender, EventArgs e)
         {
-
-
             // Obtener la hora actual
             DateTime now = DateTime.Now;
 
-            // Formatear la hora actual como una cadena
+            // Darle forma a la hora actual
             string horaActual = now.ToString("HH:mm:ss");
 
             // Construir el mensaje de bienvenida
             string mensajeBienvenida = "            Bienvenido/a.\nLa hora actual es: " + horaActual;
 
-                // Mostrar el mensaje de bienvenida en el control Label
-                lblBienvenida.Text = mensajeBienvenida;
-            
+            // Mostrar el mensaje de bienvenida en el control Label
+            lblBienvenida.Text = mensajeBienvenida;
+        }
 
+        private void btnIngresar_Click(object sender, EventArgs e)
+        {
+            string nombreUsuario = TxtUsuario.Text;
+            string contraseña = TxtContrasena.Text;
+
+            // Establecer la conexión con SQL Server
+            string connectionString = "server=LAPTOP-I1BSF5OM\\SQLEXPRESS ; database=PuntoVenta ; integrated security = true";// Data Source=server;Initial Catalog=database;User ID=username;Password=password;";
+            SqlConnection connection = new SqlConnection(connectionString);
+            {
+                try
+                {
+                    connection.Open();
+                    string query = "SELECT TipoUser FROM Usuarios WHERE Nom_User = @NombreUsuario AND Contra = @Contraseña";
+                    SqlCommand command = new SqlCommand(query, connection);
+                    command.Parameters.AddWithValue("@NombreUsuario", nombreUsuario);
+                    command.Parameters.AddWithValue("@Contraseña", contraseña);
+
+                    object tipoUsuario = command.ExecuteScalar();
+
+                    if (tipoUsuario != null)
+                    {
+                        // El usuario ha iniciado sesión correctamente
+                        string tipoUsuarioString = tipoUsuario.ToString();                   
+                        switch (tipoUsuarioString)
+                        {
+                            case "Administrador":
+                                // Lógica para el usuario administrador
+                                MessageBox.Show("Si entras perrio");
+                                Limpiar();
+                                break;
+                            case "Empleado":
+                                // Lógica para el usuario normal
+                                break;                           
+                        }
+                    }
+                    else
+                    {                    
+                        MessageBox.Show("Nombre de usuario o contraseña incorrectos.");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Manejar cualquier error de conexión
+                    MessageBox.Show("Error de conexión: " + ex.Message);
+                }
+            }
+        }
+        public void Limpiar()
+        {
+          
+            TxtContrasena.Text = string.Empty;
+            TxtUsuario.Text = string.Empty;
+
+        }
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
         }
     }
 }
